a5fc53dbe763fc81182478e679bd881e2f8285d5
Chet G <chetgassett@gmail.com>
2014-04-18 11:55:01 -0500

    second commit

---
+++ clickSlide.jquery.js
@@ -0,0 +1,32 @@
+(function($){
+  $.fn.clickSlide = function(){
+  //Set blockTitle var as child of THIS.
+    var blockTitle = $(this).children('li').children('.blockTitle');
+  //function to generate data-height attribute and default height to 0.
+    blockTitle.siblings('ul').each(function(){
+      $(this).attr('data-height',$(this).height());
+    }).height('0');
+  //Read More Hover Fade
+    blockTitle.hover(function(){
+      $(this).children('.readMore').fadeIn();
+    }, function(){
+      $(this).children('.readMore').fadeOut();
+    });
+  //Bind Click event to all .blockTitle elements
+    blockTitle.click(function(){
+  //UL are used as the inside drawer. 
+      var ul = $(this).siblings('ul'),
+          height = ul.attr('data-height');
+  //Close any drawers that have selected class then remove class on click.
+    $('.selected').siblings().animate({height:'0'},200).css('padding','0').removeClass('selected');
+  //Open and close the drawer based on height.
+    if(ul.height() > 0){
+      ul.animate({height: '0'}).css('padding','0');
+      $(this).removeClass('selected');
+    } else {
+      $(this).addClass('selected');
+      ul.animate({height: ''+height+''}).css('padding','15px 10px');
+    }
+    });
+  };
+}(jQuery));
